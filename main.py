894a57b0ff287502869dfa755b3eef82303d7e02
alexborda <alexandeer.borda@gmail.com>
2025-02-25 14:58:30 -0400

    nos mudamos a fly

--- main.py
+++ main.py
@@ -264,4 +264,4 @@ async def websocket_orders(websocket: WebSocket):
         print(f"Error en Orders WebSocket: {e}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
